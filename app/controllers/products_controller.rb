ae25cbcd65923542cfca42cf794b0078d2d4fcee
Fahad Al-mubarak <fahadalmubarak1@outlook.com>
2024-06-27 18:21:30 +0100

    added product and productcategory models as well as product controller and views and added a basic product card desgin

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,15 @@
+class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_product, only: [:show]
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  private
+  def set_product
+    @product = Product.find(params[:id])
+  end
+end
